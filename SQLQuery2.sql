0d0877a2fb12f4dc7e6881fd3dd605c824fc17c7
Cesar Ortega <cesarortegafrias@gmail.com>
2022-05-24 14:37:01 -0500

    Update SQLQuery2.sql

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,2 +1,4 @@
 ﻿SELECT * FROM Login
 WHERE cashier = 'Alice' AND password = '123';
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//Commands used to call data in the database table where the login information for the cashier and password information is stored and needed to procced to the next form.  
